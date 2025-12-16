61b90e74dbb4df194ba13b843188ede499859820
Frédéric Hénouille <frederic.henouille@soprasteria.com>
2025-12-17 00:34:37 +0100

    fix: update API base URL in deployment workflow and footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,7 +6,9 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <AntFooter style={{ textAlign: 'center' }}>Zoom Meetings ©{currentYear} - v1.0.1</AntFooter>
+    <AntFooter style={{ textAlign: 'center' }}>
+      Zoom Meetings ©{currentYear} - v1.0.1 (Unraid)
+    </AntFooter>
   );
 }
 
